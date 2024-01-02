1e0e4e18122e09aada2f505b50b0bd5a68d2a591
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-01-02 11:50:15 -0300

    Prescindir de las páginas web de prueba

--- simple-page/test/enterprise/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-import EnterpriseList from "../../components/EnterpriseList.js";
-(async () => {
-    const list = await EnterpriseList.load();
-    document.querySelector("#root").append(list.render());
-})();
