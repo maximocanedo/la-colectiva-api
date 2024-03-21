26c5c6a5f705bf3ec6450c84e28a4c64a9806f5a
maximocanedo <maximotomascanedo@gmail.com>
2024-03-20 22:50:04 -0300

    Cambios mínimos

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import * as x from './validators/index';
 import V from "./validators/index";
 import rateLimit from "express-rate-limit";
 
-const useSSL: boolean = true;
+const useSSL: boolean = false;
 
 // Cargar los certificados SSL
 let options: any = {};
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 app.use(rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hora
-    limit: 1000
+    limit: 10000
 }));
 
 app.set('json spaces', 4);
@@ -69,4 +69,6 @@ app.use("/paths", routes.paths);
 app.use("/availabilities", routes.availabilities);
 app.use("/schedules", routes.schedules);
 app.use("/query", routes.query);
+app.use("/reports", routes.reports);
+
 
